c4f47e730800dedad195e07610126a61d2b88971
kraken1970 <kraken@hi.ru>
2018-02-02 12:07:48 +0300

    1-st commit

---
+++ public/javascripts/common.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+console.log('test2010');
+$.stellar({
+	parallaxBackgrounds: true,
+	responsive: false
+});
+
+	function wResize() {
+		$("header").css("min-height", $(window).height())
+	};
+	wResize();
+	$window.resize(function() {
+		wResize()
+	});
+	//Цели для Яндекс.Метрики и Google Analytics
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//Аякс отправка форм
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("#form").submit(function(e) {
+		e.preventDefault;
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+	});
+	
+});
